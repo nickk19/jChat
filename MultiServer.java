4c505600e6c0e4937a8bbea82053be6469d0fa69
Nico <74491698+nickk19@users.noreply.github.com>
2022-12-06 20:37:34 +0100

    Add files via upload

---
+++ MultiServer.java
@@ -0,0 +1,35 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class MultiServer {
+	public static ArrayList<Socket> connections = new ArrayList<>();
+	
+	public void start() {
+		try (ServerSocket serverSocket = new ServerSocket(6789)) {
+			System.out.println("Server is waiting...\n");
+			System.out.println("Connections number: " + connections.size());
+
+			while (true) {
+				Socket socket = serverSocket.accept();
+				connections.add(socket);
+				System.out.println(socket.getInetAddress().getCanonicalHostName() + " connected");
+				System.out.println("Connections number: " + connections.size());
+
+				ServerRunnable serverRunnable = new ServerRunnable(socket);
+				Thread serverThread = new Thread(serverRunnable);
+				serverThread.start();
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Error while starting server.\n");
+			System.exit(1);
+		}
+	}
+
+	public static void main(String[] args) {
+		MultiServer tcpServer = new MultiServer();
+		tcpServer.start();
+	}
+}
